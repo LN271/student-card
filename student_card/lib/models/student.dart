8519d53dd0eae23dbf0e0ff7a0036c3b781f2150
Your Name <you@example.com>
2025-12-22 18:42:55 +0300

    step1 code enter info

---
+++ student_card/lib/models/student.dart
@@ -0,0 +1,13 @@
+class Student {
+  final String id;
+  final String name;
+  final String department;
+  final String? phoneNumber;
+
+  Student({
+    required this.id,
+    required this.name,
+    required this.phoneNumber,
+    required this.department,
+  });
+}
---
+++ student_card/lib/screen_one/enter_info.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class EnterInfo extends StatefulWidget {
+
+  @override
+  Stack<EnterInfo> createState() => _EnterInfoState();
+}
+class _EnterInfoState extends State<EnterInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Enter Information Student'),
+      ),
+      body: 
+    );
+  }
+}
