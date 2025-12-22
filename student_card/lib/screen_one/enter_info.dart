fb1debb5f8a27dcab72714cc6bf4120160c3253c
Your Name <you@example.com>
2025-12-22 20:09:20 +0300

    step3 create screencard

--- student_card/lib/screen_one/enter_info.dart
+++ student_card/lib/screen_one/enter_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:student_card/models/student.dart';
 
 class EnterInfo extends StatefulWidget {
   @override
@@ -6,6 +7,7 @@ class EnterInfo extends StatefulWidget {
 }
 
 class _EnterInfoState extends State<EnterInfo> {
+  final _fromKey = GlobalKey<FormState>();
   final NameStudent = TextEditingController();
   final IDstudent = TextEditingController();
   final PhoneNumber = TextEditingController();
@@ -26,80 +28,99 @@ class _EnterInfoState extends State<EnterInfo> {
             ),
           ),
           Container(
-            child: SingleChildScrollView(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.8),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.all(20),
-                      child: TextFormField(
-                        controller: NameStudent,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              key: _fromKey,
+              child: SingleChildScrollView(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.8),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.all(20),
+                        child: TextFormField(
+                          controller: NameStudent,
+                          decoration: const InputDecoration(
+                            labelText: 'Name Student',
+                          ),
+                          validator:
+                              (v) =>
+                                  v!.isEmpty ? 'Please enter your name' : null,
+                        ),
+                      ),
+                      TextFormField(
+                        controller: IDstudent,
                         decoration: const InputDecoration(
-                          labelText: 'Name Student',
+                          labelText: 'ID Student',
                         ),
-                        validator:
-                            (v) => v!.isEmpty ? 'Please enter your name' : null,
+                        validator: (v) {
+                          if (v!.length < 1 || v.length > 9) {
+                            return ' must enter ID student';
+                          }
+                          return null;
+                        },
                       ),
-                    ),
-                    TextFormField(
-                      controller: IDstudent,
-                      decoration: const InputDecoration(
-                        labelText: 'ID Student',
+                      Padding(padding: EdgeInsets.all(2)),
+                      TextFormField(
+                        controller: PhoneNumber,
+                        decoration: const InputDecoration(
+                          labelText: 'Phone number',
+                        ),
+                        validator: (v) {
+                          if (v!.length != 11) {
+                            return ' Enter phone number';
+                          }
+                          return null;
+                        },
                       ),
-                      validator: (v) {
-                        if (v!.length < 1 || v.length > 9) {
-                          return ' must enter ID student';
-                        }
-                        return null;
-                      },
-                    ),
-                    Padding(padding: EdgeInsets.all(2)),
-                    TextFormField(
-                      controller: PhoneNumber,
-                      decoration: const InputDecoration(
-                        labelText: 'Phone number',
+                      Padding(padding: EdgeInsets.all(2)),
+                      DropdownButtonFormField(
+                        value: Department,
+                        items: const [
+                          DropdownMenuItem(
+                            value: 'Information Technology',
+                            child: Text('Information Technology'),
+                          ),
+                          DropdownMenuItem(
+                            value: 'Medicine',
+                            child: Text('Medicine'),
+                          ),
+                          DropdownMenuItem(value: 'Law', child: Text('Law')),
+                          DropdownMenuItem(
+                            value: 'Nursing',
+                            child: Text('Nursing'),
+                          ),
+                          DropdownMenuItem(
+                            value: 'Pharmacy',
+                            child: Text('Pharmacy'),
+                          ),
+                        ],
+                        onChanged: (value) {
+                          setState(() {
+                            Department = value.toString();
+                          });
+                        },
                       ),
-                      validator: (v) {
-                        if (v!.length != 11) {
-                          return ' Enter phone number';
-                        }
-                        return null;
-                      },
-                    ),
-                    Padding(padding: EdgeInsets.all(2)),
-                    DropdownButtonFormField(
-                      value: Department,
-                      items: const [
-                        DropdownMenuItem(
-                          value: 'Information Technology',
-                          child: Text('Information Technology'),
-                        ),
-                        DropdownMenuItem(
-                          value: 'Medicine',
-                          child: Text('Medicine'),
-                        ),
-                        DropdownMenuItem(value: 'Law', child: Text('Law')),
-                        DropdownMenuItem(
-                          value: 'Nursing',
-                          child: Text('Nursing'),
-                        ),
-                        DropdownMenuItem(
-                          value: 'Pharmacy',
-                          child: Text('Pharmacy'),
-                        ),
-                      ],
-                      onChanged: (value) {
-                        setState(() {
-                          Department = value.toString();
-                        });
-                      },
-                    ),
-                    
-                  ],
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_fromKey.currentState!.validate()) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (_)=>ScreenCard(
+                                Student(id: IDstudent.text, name: NameStudent.text, phoneNumber: PhoneNumber.text, department: Department)
+
+                              ),),
+                            );
+                          }
+                        },
+                        child: const Text("submit"),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
