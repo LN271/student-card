2ced3419d733c1901cdf531970f31bb9e7250a84
Your Name <you@example.com>
2025-12-22 22:53:51 +0300

    step5

--- student_card/lib/screen_one/Screen_Card.dart
+++ student_card/lib/screen_one/Screen_Card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import '../models/Student.dart';
+
 class ScreenCard extends StatelessWidget {
   final Student student;
   const ScreenCard({super.key, required this.student});
@@ -7,45 +9,86 @@ class ScreenCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Student Card")),
-      body: LayoutBuilder(
-        builder: (context, costraints) {
-          double width = costraints.maxWidth > 500 ? 400 : 320;
-          return Card(elevation: 8,
-          shape:  RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
+      body: Center(
+        child: SingleChildScrollView(
+          child: LayoutBuilder(
+            builder: (context, costraints) {
+              double cardwidth;
+              if (costraints.maxWidth > 600) {
+                cardwidth = 500;
+              } else {
+                cardwidth = costraints.maxWidth * 0.9;
+              }
+              return Container(
+                width: cardwidth,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 240, 240, 240),
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Card(
+                  elevation: 8,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            FlutterLogo(),
+                            Container(
+                              padding: EdgeInsets.all(8),
+                              child: const Text(
+                                "University of Nineveh\n      aaasdaskfdskfds       \n      sdfdofpda",
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            FlutterLogo(),
+                          ],
+                        ),
+                        const Divider(),
+                        ListTile(
+                          leading: const Icon(Icons.person),
+                          title: Text(student.name),
+                          subtitle: const Text("Name Student"),
+                        ),
+                        ListTile(
+                          leading: const Icon(Icons.badge),
+                          title: Text(student.id),
+                          subtitle: const Text("Student ID"),
+                        ),
+                        ListTile(
+                          leading: const Icon(Icons.school),
+                          title: Text(student.department),
+                          subtitle: const Text("Department"),
+                        ),
+                        ListTile(
+                          leading: const Icon(Icons.person),
+                          title: Text(student.phoneNumber ?? "N/A"),
+                          subtitle: const Text("Name Student"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
           ),
-          child: Container(
-            width: width,
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text("University of Nineveh",style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold ),),
-                const Divider(),
-                ListTile(
-                  leading: const Icon(Icons.person),
-                  title: Text(student.name),
-                  subtitle: const Text("Name Student"),
-                ),
-                ListTile(
-                  leading: const Icon(Icons.badge),
-                  title: Text(student.id),
-                  subtitle: const Text("Student ID"),
-                ),
-                ListTile(
-                  leading: const Icon(Icons.school),
-                  title: Text(student.department),
-                  subtitle: const Text("Department"),
-                ),
-                ListTile(
-                  leading: const Icon(Icons.person),
-                  title: Text(student.phoneNumber ?? "N/A"),
-                  subtitle: const Text("Name Student"),
-                ),
-              ],
-            ),
-          ),);
-        },
+        ),
       ),
     );
   }
--- student_card/lib/screen_one/enter_info.dart
+++ student_card/lib/screen_one/enter_info.dart
@@ -3,6 +3,8 @@ import '../screen_one/Screen_Card.dart';
 import '../models/Student.dart';
 
 class EnterInfo extends StatefulWidget {
+  const EnterInfo({super.key});
+
   @override
   State<EnterInfo> createState() => _EnterInfoState();
 }
@@ -12,11 +14,14 @@ class _EnterInfoState extends State<EnterInfo> {
   final NameStudent = TextEditingController();
   final IDstudent = TextEditingController();
   final PhoneNumber = TextEditingController();
-  String Department = 'Information Technology';
+  String? Department;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Enter Information Student')),
+      appBar: AppBar(
+        title: Text('Enter Information Student'),
+        centerTitle: true,
+      ),
       body: Stack(
         children: [
           Container(
@@ -31,103 +36,116 @@ class _EnterInfoState extends State<EnterInfo> {
           Container(
             child: Padding(
               padding: const EdgeInsets.all(8.0),
-              key: _fromKey,
-              child: SingleChildScrollView(
+              child: Form(
+                key: _fromKey,
+
                 child: Container(
                   decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.8),
+                    color: Colors.white.withOpacity(0.4),
                     borderRadius: BorderRadius.circular(10),
                   ),
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.all(20),
-                        child: TextFormField(
-                          controller: NameStudent,
+                  child: SingleChildScrollView(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Container(
+                          child: TextFormField(
+                            controller: NameStudent,
+                            decoration: const InputDecoration(
+                              labelText: 'Name Student',
+                            ),
+                            validator:
+                                (v) =>
+                                    v!.isEmpty
+                                        ? 'Please enter your name'
+                                        : null,
+                          ),
+                        ),
+                        TextFormField(
+                          controller: IDstudent,
+                          keyboardType: TextInputType.number,
                           decoration: const InputDecoration(
-                            labelText: 'Name Student',
+                            labelText: 'ID Student',
                           ),
+                          validator: (v) {
+                            if (v == null || v.isEmpty || v.length > 9) {
+                              return ' must enter ID student';
+                            }
+                            return null;
+                          },
+                        ),
+                        Padding(padding: EdgeInsets.all(2)),
+                        TextFormField(
+                          controller: PhoneNumber,
+                          keyboardType: TextInputType.phone,
+                          decoration: const InputDecoration(
+                            labelText: 'Phone number',
+                          ),
+                          validator: (v) {
+                            if (v == null || v.length != 11) {
+                              return ' Enter phone number';
+                            }
+                            return null;
+                          },
+                        ),
+                        Padding(padding: EdgeInsets.all(2)),
+                        DropdownButtonFormField<String>(
+                          // hint: const Text('Select Department'),
+                          value: Department,
+                          decoration: const InputDecoration(
+                            labelText: "Department",
+                          ),
+                          items: const [
+                            DropdownMenuItem(
+                              value: 'IT',
+                              child: Text('Information Technology'),
+                            ),
+                            DropdownMenuItem(
+                              value: 'Medicine',
+                              child: Text('Medicine'),
+                            ),
+                            DropdownMenuItem(value: 'Law', child: Text('Law')),
+                            DropdownMenuItem(
+                              value: 'Nursing',
+                              child: Text('Nursing'),
+                            ),
+                            DropdownMenuItem(
+                              value: 'Pharmacy',
+                              child: Text('Pharmacy'),
+                            ),
+                          ],
+                          onChanged:
+                              (value) => setState(() => Department = value),
                           validator:
-                              (v) =>
-                                  v!.isEmpty ? 'Please enter your name' : null,
+                              (value) =>
+                                  value == null
+                                      ? 'Please select a department'
+                                      : null,
                         ),
-                      ),
-                      TextFormField(
-                        controller: IDstudent,
-                        decoration: const InputDecoration(
-                          labelText: 'ID Student',
-                        ),
-                        validator: (v) {
-                          if (v!.length < 1 || v.length > 9) {
-                            return ' must enter ID student';
-                          }
-                          return null;
-                        },
-                      ),
-                      Padding(padding: EdgeInsets.all(2)),
-                      TextFormField(
-                        controller: PhoneNumber,
-                        decoration: const InputDecoration(
-                          labelText: 'Phone number',
-                        ),
-                        validator: (v) {
-                          if (v!.length != 11) {
-                            return ' Enter phone number';
-                          }
-                          return null;
-                        },
-                      ),
-                      Padding(padding: EdgeInsets.all(2)),
-                      DropdownButtonFormField(
-                        value: Department,
-                        items: const [
-                          DropdownMenuItem(
-                            value: 'Information Technology',
-                            child: Text('Information Technology'),
-                          ),
-                          DropdownMenuItem(
-                            value: 'Medicine',
-                            child: Text('Medicine'),
-                          ),
-                          DropdownMenuItem(value: 'Law', child: Text('Law')),
-                          DropdownMenuItem(
-                            value: 'Nursing',
-                            child: Text('Nursing'),
-                          ),
-                          DropdownMenuItem(
-                            value: 'Pharmacy',
-                            child: Text('Pharmacy'),
-                          ),
-                        ],
-                        onChanged: (value) {
-                          setState(() {
-                            Department = value.toString();
-                          });
-                        },
-                      ),
-                      const SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: () {
-                          if (_fromKey.currentState!.validate()) {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder:
-                                    (_) => ScreenCard(
-                                      student: Student(
-                                        id: IDstudent.text,
-                                        name: NameStudent.text,
-                                        phoneNumber: PhoneNumber.text,
-                                        department: Department,
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (_fromKey.currentState!.validate()) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder:
+                                      (_) => ScreenCard(
+                                        student: Student(
+                                          id: IDstudent.text,
+                                          name: NameStudent.text,
+                                          phoneNumber: PhoneNumber.text,
+                                          department: Department!,
+                                        ),
                                       ),
-                                    ),
-                              ),
-                            );
-                          }
-                        },
-                        child: const Text("submit"),
-                      ),
-                    ],
+                                ),
+                              );
+                            }
+                          },
+                          child: const Text("submit"),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
