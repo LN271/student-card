66ef5d698321d2f2c6406523d0a84cd541051c9e
Your Name <you@example.com>
2025-12-22 19:57:18 +0300

    step2 crate inf to card

--- student_card/lib/screen_one/enter_info.dart
+++ student_card/lib/screen_one/enter_info.dart
@@ -1,18 +1,111 @@
 import 'package:flutter/material.dart';
 
 class EnterInfo extends StatefulWidget {
-
   @override
-  Stack<EnterInfo> createState() => _EnterInfoState();
+  State<EnterInfo> createState() => _EnterInfoState();
 }
+
 class _EnterInfoState extends State<EnterInfo> {
+  final NameStudent = TextEditingController();
+  final IDstudent = TextEditingController();
+  final PhoneNumber = TextEditingController();
+  String Department = 'Information Technology';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Enter Information Student'),
+      appBar: AppBar(title: Text('Enter Information Student')),
+      body: Stack(
+        children: [
+          Container(
+            child: Image.asset(
+              'images/n.jpg',
+              fit: BoxFit.cover,
+              height: double.infinity,
+              width: double.infinity,
+              alignment: Alignment.center,
+            ),
+          ),
+          Container(
+            child: SingleChildScrollView(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(20),
+                      child: TextFormField(
+                        controller: NameStudent,
+                        decoration: const InputDecoration(
+                          labelText: 'Name Student',
+                        ),
+                        validator:
+                            (v) => v!.isEmpty ? 'Please enter your name' : null,
+                      ),
+                    ),
+                    TextFormField(
+                      controller: IDstudent,
+                      decoration: const InputDecoration(
+                        labelText: 'ID Student',
+                      ),
+                      validator: (v) {
+                        if (v!.length < 1 || v.length > 9) {
+                          return ' must enter ID student';
+                        }
+                        return null;
+                      },
+                    ),
+                    Padding(padding: EdgeInsets.all(2)),
+                    TextFormField(
+                      controller: PhoneNumber,
+                      decoration: const InputDecoration(
+                        labelText: 'Phone number',
+                      ),
+                      validator: (v) {
+                        if (v!.length != 11) {
+                          return ' Enter phone number';
+                        }
+                        return null;
+                      },
+                    ),
+                    Padding(padding: EdgeInsets.all(2)),
+                    DropdownButtonFormField(
+                      value: Department,
+                      items: const [
+                        DropdownMenuItem(
+                          value: 'Information Technology',
+                          child: Text('Information Technology'),
+                        ),
+                        DropdownMenuItem(
+                          value: 'Medicine',
+                          child: Text('Medicine'),
+                        ),
+                        DropdownMenuItem(value: 'Law', child: Text('Law')),
+                        DropdownMenuItem(
+                          value: 'Nursing',
+                          child: Text('Nursing'),
+                        ),
+                        DropdownMenuItem(
+                          value: 'Pharmacy',
+                          child: Text('Pharmacy'),
+                        ),
+                      ],
+                      onChanged: (value) {
+                        setState(() {
+                          Department = value.toString();
+                        });
+                      },
+                    ),
+                    
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      body: 
     );
   }
 }
