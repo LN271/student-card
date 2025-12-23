740b249ae50102036434353f8646903da205624d
Your Name <you@example.com>
2025-12-23 13:22:57 +0300

    step5 updata the code

--- student_card/lib/main.dart
+++ student_card/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screen_one/enter_info.dart';
+import 'screen_one/Screen_Card.dart';
+import '../models/Student.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +16,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Student ID ',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: EnterInfo(),
+      home: ScreenCard(
+        student: Student(
+          id: "123456789",
+          name: "name",
+          phoneNumber: "09876543211",
+          department: "department",
+        ),
+      ),
     );
   }
 }
